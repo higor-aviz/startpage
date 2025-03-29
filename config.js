7fcff819239827f4fb18b7ab12a573d5437c4b95
higor-aviz <higor.aviz@proton.me>
2025-03-28 22:28:37 -0300

    Update config.js

--- config.js
+++ config.js
@@ -65,9 +65,9 @@ const CONFIG = {
 		},
 		{
 			id: '3',
-			name: 'Todoist',
-			icon: 'trello',
-			link: 'https://todoist.com',
+			name: 'ChatGPT',
+			icon: 'bot',
+			link: 'https://chatgpt.com',
 		},
 		{
 			id: '4',
